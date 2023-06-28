b129700734476ea1aecdeb017bfcabc1c4749b45
Jão <joaohenriquerc123@gmail.com>
2023-06-28 10:30:47 -0300

    Finalizado

--- js/script.js
+++ js/script.js
@@ -3,23 +3,78 @@ const pokemonNum = document.querySelector('.pokemon-num');
 const pokemonImg = document.querySelector('.pokemon-img');
 
 const form = document.querySelector('.busca');
+const input = document.querySelector('.busca-input');
+const prev = document.querySelector('.btn-prev');
+const next = document.querySelector('.btn-next');
+
+let buscaPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
 
     const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data = await APIresponse.json();
 
-    return data;
+    if(APIresponse.status == 200){
 
+        const data = await APIresponse.json();
+
+        return data;
+    }
 }
 
 const renderPokemon = async (pokemon) => {
 
+    pokemonNome.innerHTML = 'Loading...';
+    pokemonNum.innerHTML = '';
+
     const data = await fetchPokemon(pokemon);
 
-    pokemonNome.innerHTML = data.name;
-    pokemonNum.innerHTML = data.id;
-    pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    if(data){
+        pokemonImg.style.display = 'block';
+        pokemonNome.innerHTML = data.name;
+        pokemonNum.innerHTML = data.id;
+        pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    
+        buscaPokemon = data.id;
+
+        input.value = '';
+
+    }else{
+
+        pokemonImg.style.display = 'none';
+        pokemonNome.innerHTML = 'Not Found';
+        pokemonNum.innerHTML = '';
+
+        input.value = '';
+
+    }
+
 }
 
-renderPokemon('150')    
+form.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+
+    renderPokemon(input.value.toLowerCase()); 
+});
+
+prev.addEventListener('click', (event) => {
+
+    if(buscaPokemon > 1){
+
+        buscaPokemon -= 1;
+    
+        renderPokemon(buscaPokemon);
+    }
+
+});
+
+next.addEventListener('click', (event) => {
+
+    buscaPokemon += 1;
+
+    renderPokemon(buscaPokemon);
+});
+
+
+
+renderPokemon(buscaPokemon);
